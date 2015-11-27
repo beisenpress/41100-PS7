ccd6af9da3a2517a50c342d24687ec444c4921ee
beisenpress <ben@eisenpress.com>
2015-11-27 12:09:00 -0800

    Added Question 2

--- PS7.R
+++ PS7.R
@@ -116,9 +116,102 @@ yelp <- read.csv("yelp.csv")
 # (review_count) as a control variable. Comment on any differences in the conclusions from these 
 # two models.
 
-# install.packages("nnet")
+#install.packages("nnet")
 library(MASS)
 library(nnet)
 
+# Convert Stars variable from numeric to character
+yelp$stars1 <- factor(yelp$stars)
+
+# Create new dataset without numeric stars variable
+yelp1 <- yelp[,2:ncol(yelp)]
+
 # Do Ordered logistic regression
-yelp.reg1 <- polr(stars ~ Price.Range + Delivery, data=yelp, Hess=TRUE)
+yelp.reg1 <- polr(stars1 ~ ., data=yelp1, Hess=TRUE)
+yelp.reg2 <- polr(stars1 ~ . - review_count, data=yelp1, Hess=TRUE)
+
+# We cannot run a logistic regression using the number of reviews because that uniquely 
+# identifies restaurants.
+
+# (c) Using your favorite model selection technique construct a model to predict star ratings, 
+# considering main effects and interactions, but excluding review count. Use a test/training 
+# approach to validate your model and compare it to the others you’ve fit (use MSE to compare 
+# models). Interpret your findings.
+
+# Remove review count from dataset
+yelp2 <- yelp1[,2:ncol(yelp1)]
+
+# Set seed so the results are replicable 
+set.seed(94)
+
+# Select a random sample of rows
+samples <- sort(sample.int(nrow(yelp2), 0.80*nrow(yelp2)))
+
+# Subset the data into training and test datasets.
+yelp2.train <- yelp2[samples,] 
+yelp2.test <- yelp2[-samples,]
+
+# Use forward stepwise selection under BIC to select a model.
+
+#Create the null regression
+yelp.reg.null <- polr(stars1 ~ 1, data=yelp2.train, Hess=TRUE)
+
+#Create the full regression
+yelp.reg.full <- polr(stars1 ~ . + .^2, data=yelp2.train, Hess=TRUE)
+
+# Run forward stepwise BIC
+yelp.reg.BIC <- step(yelp.reg.null, scope=formula(yelp.reg.full), direction="forward", k=log(nrow(yelp2)))
+
+# Forward stepwise regression takes a long time to run.
+
+# Final regression is: 
+
+# Call:
+#   polr(formula = stars1 ~ Parking.street + Parking.lot + accepts.credit + 
+#          Good.For.latenight + Take.out + open + Outdoor.Seating + 
+#          Good.For.brunch + Parking.valet + Good.For.dessert + Parking.garage + 
+#          Good.For.breakfast + accepts.credit:Take.out + Parking.street:Outdoor.Seating + 
+#          Parking.lot:Outdoor.Seating + Parking.street:Parking.lot + 
+#          Parking.street:Parking.valet + Take.out:Good.For.breakfast + 
+#          Outdoor.Seating:Good.For.breakfast + Parking.lot:Good.For.brunch + 
+#          Parking.street:open + Parking.lot:open, data = yelp2.train, 
+#        Hess = TRUE)
+# 
+# Coefficients:
+#   Value Std. Error t value
+# Parking.streetTRUE                          0.63620    0.12774  4.9805
+# Parking.lotTRUE                             0.36859    0.09412  3.9163
+# accepts.creditTRUE                          0.43678    0.25677  1.7011
+# Good.For.latenightTRUE                     -0.58981    0.06718 -8.7797
+# Take.outTRUE                                1.02711    0.27044  3.7979
+# openTRUE                                   -0.05003    0.08006 -0.6249
+# Outdoor.SeatingTRUE                         0.38376    0.05971  6.4274
+# Good.For.brunchTRUE                         0.61282    0.10177  6.0215
+# Parking.valetTRUE                           0.59066    0.09689  6.0960
+# Good.For.dessertTRUE                        0.51660    0.12404  4.1647
+# Parking.garageTRUE                         -0.21898    0.06700 -3.2682
+# Good.For.breakfastTRUE                     -1.39697    0.24963 -5.5962
+# accepts.creditTRUE:Take.outTRUE            -1.55111    0.27720 -5.5956
+# Parking.streetTRUE:Outdoor.SeatingTRUE     -0.53183    0.09466 -5.6183
+# Parking.lotTRUE:Outdoor.SeatingTRUE        -0.33494    0.07198 -4.6532
+# Parking.streetTRUE:Parking.lotTRUE         -0.35813    0.12467 -2.8725
+# Parking.streetTRUE:Parking.valetTRUE       -0.79777    0.22520 -3.5425
+# Take.outTRUE:Good.For.breakfastTRUE         1.08206    0.25387  4.2622
+# Outdoor.SeatingTRUE:Good.For.breakfastTRUE  0.42599    0.11217  3.7978
+# Parking.lotTRUE:Good.For.brunchTRUE        -0.50496    0.13873 -3.6399
+# Parking.streetTRUE:openTRUE                 0.59302    0.12530  4.7328
+# Parking.lotTRUE:openTRUE                    0.44020    0.09466  4.6503
+# 
+# Intercepts:
+#   Value    Std. Error t value 
+# 1|1.5  -6.7333   0.4100   -16.4232
+# 1.5|2  -4.3608   0.2796   -15.5989
+# 2|2.5  -2.8745   0.2664   -10.7913
+# 2.5|3  -1.5702   0.2635    -5.9587
+# 3|3.5  -0.3688   0.2630    -1.4024
+# 3.5|4   0.9330   0.2631     3.5464
+# 4|4.5   2.6773   0.2642    10.1334
+# 4.5|5   5.1269   0.2761    18.5688
+# 
+# Residual Deviance: 40664.20 
+# AIC: 40724.20 
